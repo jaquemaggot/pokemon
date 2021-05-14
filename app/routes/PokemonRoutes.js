ff3eac786b3ab6057c535c9960eb227a4b3c319b
Jaquemaggot <jaquem1618@gmail.com>
2021-05-14 20:12:53 -0300

    versao inicial

---
+++ app/routes/PokemonRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+//Indentifica as rotas que estão chegando
+const router = express.Router();
+//const PokemonController = require('../controller/PokemonController');
+
+router.post('/',)
+
+module.exports = router;
